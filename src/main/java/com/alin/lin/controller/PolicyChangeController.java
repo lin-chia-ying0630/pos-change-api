02544b19dfdf2950c5dd7f5a364db93a0473d920
linjiaying <s290510790@gmail.com>
2026-07-10 19:24:10 +0800

    Allow Docker web origin for policy APIs

--- src/main/java/com/alin/lin/controller/PolicyChangeController.java
+++ src/main/java/com/alin/lin/controller/PolicyChangeController.java
@@ -30,8 +30,10 @@ import java.util.List;
 @CrossOrigin(origins = {
         "http://localhost:5173",
         "http://localhost:5174",
+        "http://localhost:8080",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174"
+        "http://127.0.0.1:5174",
+        "http://127.0.0.1:8080"
 })
 @RestController
 @RequestMapping("/api")
